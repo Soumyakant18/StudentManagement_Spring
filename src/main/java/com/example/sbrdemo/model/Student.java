76a363c9bca43a55af60574a18e68cb63dc77c8d
soumyakanta rout <soumykantarout@gmail.com>
2024-07-19 12:06:34 +0530

    complete project with frontend

---
+++ src/main/java/com/example/sbrdemo/model/Student.java
@@ -0,0 +1,28 @@
+package com.example.sbrdemo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    @NaturalId(mutable = true)
+    private  String email;
+    private  String department;
+
+}
